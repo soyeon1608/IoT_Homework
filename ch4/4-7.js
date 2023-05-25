6344347c1f7f335c22af5f329407fb7e589403dd
Soyeon Lee <soyeon1608@gmail.com>
2023-05-25 17:02:51 +0900

    this

---
+++ ch4/4-7.js
@@ -0,0 +1,10 @@
+var obj = {
+  vals: [1, 2, 3],
+  logValues: function(v, i) {
+    console.log(this, v, i);
+  },
+};
+obj.logValues(1, 2); // { vals: [1, 2, 3], logValues: f } 1 2
+[4, 5, 6].forEach(obj.logValues); // Window { ... } 4 0
+// Window { ... } 5 1
+// Window { ... } 6 2
