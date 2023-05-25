cedac08a9053f72938ed5e02f38441e8f4a1ad12
Soyeon Lee <soyeon1608@gmail.com>
2023-05-25 17:23:06 +0900

    setInterval/setTimeout

---
+++ ch5/5-4.js
@@ -0,0 +1,12 @@
+// (1) setInterval/setTimeout
+(function() {
+  var a = 0;
+  var intervalId = null;
+  var inner = function() {
+    if (++a >= 10) {
+      clearInterval(intervalId);
+    }
+    console.log(a);
+  };
+  intervalId = setInterval(inner, 1000);
+})();
