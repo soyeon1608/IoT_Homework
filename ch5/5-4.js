3e75f0642329c1ca8db44025425231e313559e3f
Soyeon Lee <soyeon1608@gmail.com>
2023-05-25 17:24:27 +0900

    setInterval/setTimsetInterval, eventListener

--- ch5/5-4.js
+++ ch5/5-4.js
@@ -1,12 +1,23 @@
 // (1) setInterval/setTimeout
 (function() {
-  var a = 0;
-  var intervalId = null;
-  var inner = function() {
-    if (++a >= 10) {
-      clearInterval(intervalId);
-    }
-    console.log(a);
-  };
-  intervalId = setInterval(inner, 1000);
-})();
+    var a = 0;
+    var intervalId = null;
+    var inner = function() {
+      if (++a >= 10) {
+        clearInterval(intervalId);
+      }
+      console.log(a);
+    };
+    intervalId = setInterval(inner, 1000);
+  })();
+
+  // (2) eventListener
+  (function() {
+    var count = 0;
+    var button = document.createElement('button');
+    button.innerText = 'click';
+    button.addEventListener('click', function() {
+      console.log(++count, 'times clicked');
+    });
+    document.body.appendChild(button);
+  })();
